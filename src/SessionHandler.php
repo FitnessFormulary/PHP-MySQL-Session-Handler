10eb7ed6bbafff523882815b90a14a0e204df8e6
Rami Azzazi <formurami@icloud.com>
2019-05-28 18:08:39 -0500

    A PDO version of programster's SessionHandler

--- src/SessionHandler.php
+++ src/SessionHandler.php
@@ -1,104 +1,113 @@
 <?php
+/** This is a fork from Programster's implementation of PHP's SessionHandler implementing the abstract SessionHandler
+ *  extending the SessionHandlerInterface
+ * This is modifed for PHP DATA OBJECTS (PDO) abstraction layer for accessing databases by Rami Azzazi @ramiazzazi
+ **/
 
 namespace Programster\SessionHandler;
-
+use PDO;
 
 final class SessionHandler implements \SessionHandlerInterface
 {
     private $dbConnection; # the mysqli connection
     private $dbTable; # name of the db table to store sessions in
     private $m_maxAge;
-    
-    
+
+
     /**
      * Create the session handler.
      * @param \mysqli $mysqli - the database connection to store sessions in.
      * @param string $tableName - the table within the database to store session data in.
      * @param int $maxAge - the maximum age in seconds of a session variable.
      */
-    public function __construct(\mysqli $mysqli, string $tableName, int $maxAge=86400)
+    public function __construct(PDO $pdo, string $tableName, int $maxAge=86400)
     {
-        $this->dbConnection = $mysqli;
+        $this->dbConnection = $pdo;
         $this->dbTable = $tableName;
         $this->m_maxAge = $maxAge;
-        
-        
-        $createSessionsTableQuery = 
-            "CREATE TABLE IF NOT EXISTS `" . $this->dbTable . "` (
+
+        $createSessionsTableQuery =
+            "CREATE TABLE IF NOT EXISTS {$this->dbTable} (
                 `id` varchar(32) NOT NULL,
                 `modified_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 `data` mediumtext,
                 PRIMARY KEY (`id`),
                 KEY `modified_timestamp` (`modified_timestamp`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-        
-        $this->dbConnection->query($createSessionsTableQuery);
+
+        $stmt = $this->dbConnection->prepare($createSessionsTableQuery);
+        $stmt->execute();
     }
-    
-    
+
+
     public function open($savePath, $sessionName)
     {
         $sql =
-            "DELETE FROM `" . $this->dbTable . "` " . 
-            "WHERE `modified_timestamp` < (NOW() - INTERVAL " . $this->m_maxAge . " SECOND)";
-        
-        return $this->dbConnection->query($sql);
+            "DELETE FROM {$this->dbTable} 
+             WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";
+        $stmt = $this->dbConnection->prepare($sql);
+
+        return $stmt->execute([':m_maxAge' => $this->$m_maxAge]);
+
     }
-    
-    
+
+
     public function close()
     {
-        return $this->dbConnection->close();
+        return $this->dbConnection = null;
     }
-    
-    
+
+
     public function read($id)
     {
-        $sql = 
-            "SELECT `data` " . 
-            "FROM `" . $this->dbTable . "` " . 
-            "WHERE `id` = '" . $id . "'";
-        
-        $result = $this->dbConnection->query($sql);
-        
-        if ($result === FALSE)
+        $sql =
+            "SELECT `data`  
+             FROM `" . $this->dbTable . "` 
+             WHERE `id` = :id";
+
+        $stmt = $this->dbConnection->prepare($sql);
+        $result = $stmt->execute([":id" => $id]);
+
+
+        if ($result == FALSE)
         {
             $result = "";
         }
         else
         {
-            $row = $result->fetch_assoc();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
             $result = $row['data'];
         }
-        
+
         return $result;
     }
-    
-    
+
+
     public function write($id, $data)
     {
-        $sql = "REPLACE INTO `" . $this->dbTable . "` (id, data)" . 
-               " VALUES('" . $id . "', '" . $data . "')";
-        
-        return $this->dbConnection->query($sql);
+        $sql = "REPLACE INTO {$this->dbTable} (id, data) 
+                VALUES(:id, :data)";
+        $stmt = $this->dbConnection->prepare($sql);
+        return $stmt->execute([':id' => $id, ':data' => $data]);
     }
-    
-    
+
+
     public function destroy($id)
     {
-        $sql = "DELETE FROM `" . $this->dbTable . " WHERE `id` = '" . $id . "'";
-        return $this->dbConnection->query($sql);
+        $sql = "DELETE FROM " . $this->dbTable . " WHERE id =:id";
+        $stmt = $this->dbConnection->prepare($sql);
+        return $stmt->execute();
     }
-    
-    
+
+
     public function gc($maxlifetime)
     {
-        $sql = sprintf(
-            "DELETE FROM %s WHERE `timestamp` < '%s'", 
-            $this->dbTable, 
-            time() - intval($maxlifetime)
-        );
-        
-        return $this->dbConnection->query($sql);
+        $furthestValidTime = time() - intval(maxlifetime);
+        $sql = "DELETE FROM 
+                    {$this->dbTable} 
+                WHERE modified_timestamp < :furthestValidTime";
+        $stmt = $this->dbConnection->prepare($sql);
+        return $stmt-->execute([':furthestValidTime' => $furthestValidTime]);
     }
 }
+
