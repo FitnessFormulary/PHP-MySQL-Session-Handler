4d4cbe7ac03bdbea0a9854219d38da80e001eb8c
Rami Azzazi <formurami@icloud.com>
2019-05-30 10:32:50 -0500

    Fixes for the following: 1- Read function returning a null when rows on empty resulting in immediate session closure. 2- Garbage collection function including inconsistent datatypes in the SQL statement.

--- src/SessionHandler.php
+++ src/SessionHandler.php
@@ -3,6 +3,9 @@
  * This is a fork from Programster's implementation of PHP's SessionHandler implementing the abstract SessionHandler
  *  extending the SessionHandlerInterface
  * The fork modifies the class to utilize PHP DATA OBJECTS (PDO) abstraction layer for accessing databases rather than mysqli
+ * Thise release includes a fixes for the following:
+ * 1-Read function returning a null when rows on empty resulting in immediate session closure
+ * 2-Garbage collection function including inconsistent datatypes in the SQL statement.
  **/
 
 namespace FitnessFormulary\SessionHandler;
@@ -21,20 +24,19 @@ final class SessionHandler implements \SessionHandlerInterface
      * @param string $tableName - the table within the database to store session data in.
      * @param int $maxAge - the maximum age in seconds of a session variable.
      */
-    public function __construct(PDO $pdo, string $tableName, int $maxAge=86400)
+    public function __construct(PDO $pdo, string $tableName, int $maxAge = 86400)
     {
         $this->dbConnection = $pdo;
         $this->dbTable = $tableName;
         $this->m_maxAge = $maxAge;
 
-        $createSessionsTableQuery =
-            "CREATE TABLE IF NOT EXISTS {$this->dbTable} (
+        $createSessionsTableQuery = "
+            CREATE TABLE IF NOT EXISTS {$this->dbTable} (
                 `id` varchar(32) NOT NULL,
                 `modified_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 `data` mediumtext,
                 PRIMARY KEY (`id`),
-                KEY `modified_timestamp` (`modified_timestamp`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+                KEY `modified_timestamp` (`modified_timestamp`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
         $stmt = $this->dbConnection->prepare($createSessionsTableQuery);
         $stmt->execute();
@@ -43,9 +45,9 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function open($savePath, $sessionName)
     {
-        $sql =
-            "DELETE FROM {$this->dbTable} 
-             WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";
+        $sql ="
+            DELETE FROM {$this->dbTable} 
+            WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";
         $stmt = $this->dbConnection->prepare($sql);
 
         return $stmt->execute([':m_maxAge' => $this->m_maxAge]);
@@ -61,22 +63,19 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function read($id)
     {
-        $sql =
-            "SELECT `data`  
-             FROM `" . $this->dbTable . "` 
-             WHERE `id` = :id";
+        $sql = "
+            SELECT `data`  
+            FROM `" . $this->dbTable . "` 
+            WHERE `id` = :id";
 
         $stmt = $this->dbConnection->prepare($sql);
         $result = $stmt->execute([":id" => $id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
-        if ($result == FALSE)
-        {
+        if ($result == false || $row == false) {
             $result = "";
-        }
-        else
-        {
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
             $result = $row['data'];
         }
 
@@ -86,8 +85,9 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function write($id, $data)
     {
-        $sql = "REPLACE INTO {$this->dbTable} (id, data) 
-                VALUES(:id, :data)";
+        $sql = "
+            REPLACE INTO {$this->dbTable} (id, data) 
+            VALUES(:id, :data)";
         $stmt = $this->dbConnection->prepare($sql);
         return $stmt->execute([':id' => $id, ':data' => $data]);
     }
@@ -95,7 +95,8 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function destroy($id)
     {
-        $sql = "DELETE FROM " . $this->dbTable . " WHERE id =:id";
+        $sql = "
+            DELETE FROM " . $this->dbTable . " WHERE id =:id";
         $stmt = $this->dbConnection->prepare($sql);
         return $stmt->execute();
     }
@@ -103,12 +104,11 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function gc($maxlifetime)
     {
-        $furthestValidTime = time() - intval($maxlifetime);
-        $sql = "DELETE FROM 
-                    {$this->dbTable} 
-                WHERE modified_timestamp < :furthestValidTime";
+        $sql = "    
+            DELETE FROM {$this->dbTable} 
+            WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";;
         $stmt = $this->dbConnection->prepare($sql);
-        return $stmt->execute([':furthestValidTime' => $furthestValidTime]);
+        return $stmt->execute([':m_maxAge' => $maxlifetime]);
     }
 }
 
