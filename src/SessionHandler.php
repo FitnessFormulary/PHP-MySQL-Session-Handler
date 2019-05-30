d6d541c5b7bb85223af0cfffad26c55adeab39ea
Rami Azzazi <formurami@icloud.com>
2019-05-30 13:08:11 -0500

    Fixes for the following bugs in Programster's code:  -Fixed close() to no longer return null as PHP relies on the interface function returning only true and false  -Part of the open function now is to set the db value back to PDO (from being set to null in close())

--- src/SessionHandler.php
+++ src/SessionHandler.php
@@ -3,16 +3,18 @@
  * This is a fork from Programster's implementation of PHP's SessionHandler implementing the abstract SessionHandler
  *  extending the SessionHandlerInterface
  * The fork modifies the class to utilize PHP DATA OBJECTS (PDO) abstraction layer for accessing databases rather than mysqli
- * Thise release includes a fixes for the following:
+ * In addition to converting the code from mysqli to PDO, this release includes a fixes for the following bugs in the original code:
  * 1-Read function returning a null when rows on empty resulting in immediate session closure
  * 2-Garbage collection function including inconsistent datatypes in the SQL statement.
+ * 3-Fixed close() to no longer return null as PHP relies on the interface function returning only true and false
+ * 4-Part of the open function now is to set the db value back to PDO (from being set to null in close()
  **/
 
 namespace FitnessFormulary\SessionHandler;
 use PDO;
 
 final class SessionHandler implements \SessionHandlerInterface
-{
+{   private $pdoConnection;
     private $dbConnection;
     private $dbTable;
     private $m_maxAge;
@@ -24,19 +26,21 @@ final class SessionHandler implements \SessionHandlerInterface
      * @param string $tableName - the table within the database to store session data in.
      * @param int $maxAge - the maximum age in seconds of a session variable.
      */
-    public function __construct(PDO $pdo, string $tableName, int $maxAge = 86400)
+    public function __construct(PDO $pdo, string $tableName, int $maxAge=86400)
     {
+        $this->pdoConnection = $pdo;
         $this->dbConnection = $pdo;
         $this->dbTable = $tableName;
         $this->m_maxAge = $maxAge;
 
-        $createSessionsTableQuery = "
-            CREATE TABLE IF NOT EXISTS {$this->dbTable} (
+        $createSessionsTableQuery =
+            "CREATE TABLE IF NOT EXISTS {$this->dbTable} (
                 `id` varchar(32) NOT NULL,
                 `modified_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 `data` mediumtext,
                 PRIMARY KEY (`id`),
-                KEY `modified_timestamp` (`modified_timestamp`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+                KEY `modified_timestamp` (`modified_timestamp`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
         $stmt = $this->dbConnection->prepare($createSessionsTableQuery);
         $stmt->execute();
@@ -45,37 +49,41 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function open($savePath, $sessionName)
     {
-        $sql ="
-            DELETE FROM {$this->dbTable} 
-            WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";
+        $this->dbConnection = $this->pdoConnection;
+        $sql =
+            "DELETE FROM {$this->dbTable} 
+             WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";
         $stmt = $this->dbConnection->prepare($sql);
 
         return $stmt->execute([':m_maxAge' => $this->m_maxAge]);
-
     }
 
 
     public function close()
     {
-        return $this->dbConnection = null;
+        $this->dbConnection = null;
+        return true;
     }
 
 
     public function read($id)
     {
-        $sql = "
-            SELECT `data`  
-            FROM `" . $this->dbTable . "` 
-            WHERE `id` = :id";
+        $sql =
+            "SELECT `data`  
+             FROM `" . $this->dbTable . "` 
+             WHERE `id` = :id";
 
         $stmt = $this->dbConnection->prepare($sql);
         $result = $stmt->execute([":id" => $id]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
-        if ($result == false || $row == false) {
+        if ($result == false || $row == false)
+        {
             $result = "";
-        } else {
+        }
+        else
+        {
             $result = $row['data'];
         }
 
@@ -85,9 +93,8 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function write($id, $data)
     {
-        $sql = "
-            REPLACE INTO {$this->dbTable} (id, data) 
-            VALUES(:id, :data)";
+        $sql = "REPLACE INTO {$this->dbTable} (id, data) 
+                VALUES(:id, :data)";
         $stmt = $this->dbConnection->prepare($sql);
         return $stmt->execute([':id' => $id, ':data' => $data]);
     }
@@ -95,18 +102,17 @@ final class SessionHandler implements \SessionHandlerInterface
 
     public function destroy($id)
     {
-        $sql = "
-            DELETE FROM " . $this->dbTable . " WHERE id =:id";
+        $sql = "DELETE FROM " . $this->dbTable . " WHERE id =:id";
         $stmt = $this->dbConnection->prepare($sql);
-        return $stmt->execute();
+        $y = $stmt->execute([':id' => $id]);
+        return $y;
     }
 
 
     public function gc($maxlifetime)
     {
-        $sql = "    
-            DELETE FROM {$this->dbTable} 
-            WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";;
+        $sql =     "DELETE FROM {$this->dbTable} 
+                    WHERE `modified_timestamp` < (NOW() - INTERVAL :m_maxAge SECOND)";;
         $stmt = $this->dbConnection->prepare($sql);
         return $stmt->execute([':m_maxAge' => $maxlifetime]);
     }
